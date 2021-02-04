352debf9eb747d5286bd43417163794313831dee
LuboslavBurdenski <75112386+LuboslavBurdenski@users.noreply.github.com>
2021-02-05 00:52:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 global.__basedir = __dirname;
 require('dotenv').config()
 const dbConnector = require('./config/db');
-// const mongoose = require('mongoose');
 const apiRouter = require('./router');
 const cors = require('cors');
-// const config = require('./config/config');
 const { errorHandler } = require('./utils');
 let userModel = require('./models/userModel');
 
 dbConnector()
   .then(() => {
     const config = require('./config/config');
-
     const app = require('express')();
     require('./config/express')(app);
 
@@ -19,13 +16,8 @@ dbConnector()
       origin: config.origin,
       credentials: true
     }));
-
     app.use('/api', apiRouter);
-
-   
-
     app.use(errorHandler);
-
     app.listen(config.port, console.log(`Listening on port ${config.port}!`));
   })
   .catch(console.error);
